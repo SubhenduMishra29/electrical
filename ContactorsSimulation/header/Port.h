e63d1261114f9900c44b2f3bbf3d401dde943010
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-21 15:23:54 +0530

    Update Port.h

--- ContactorsSimulation/header/Port.h
+++ ContactorsSimulation/header/Port.h
@@ -2,6 +2,7 @@
  * File: Port.h
  * Author: Subhendu Mishra
  * License: GPL
+ * Description: Definition of the Port class, representing ports in a contactor.
  */
 
 #ifndef PORT_H
@@ -10,106 +11,48 @@
 #include <string>
 
 /**
- * @brief Class representing a connection with an ID and name.
- */
-class Connection {
-private:
-    int id; /**< ID of the connection */
-    std::string name; /**< Name of the connection */
-
-public:
-    /**
-     * @brief Constructor for Connection.
-     * @param connectionId ID of the connection.
-     * @param connectionName Name of the connection.
-     */
-    Connection(int connectionId, const std::string& connectionName) 
-        : id(connectionId), name(connectionName) {}
-
-    /**
-     * @brief Getter for connection ID.
-     * @return ID of the connection.
-     */
-    int getId() const { return id; }
-
-    /**
-     * @brief Getter for connection name.
-     * @return Name of the connection.
-     */
-    std::string getName() const { return name; }
-};
-
-/**
- * @brief Class representing a port with an input and output connection, and voltage.
+ * @brief Class representing a port with input and output connections.
  */
 class Port {
 private:
-    Connection inPort; /**< Input port */
-    Connection outPort; /**< Output port */
-    double voltage; /**< Voltage of the port */
+    double inValue; /**< Input value */
+    double outValue; /**< Output value */
 
 public:
     /**
      * @brief Constructor for Port.
-     * @param inPortId ID of the input port.
-     * @param inPortName Name of the input port.
-     * @param outPortId ID of the output port.
-     * @param outPortName Name of the output port.
      */
-    Port(int inPortId, const std::string& inPortName, int outPortId, const std::string& outPortName) 
-        : inPort(inPortId, inPortName), outPort(outPortId, outPortName), voltage(0.0) {}
+    Port() : inValue(0.0), outValue(0.0) {}
 
     /**
-     * @brief Method to set the voltage of the port.
-     * @param volt Voltage to be set.
+     * @brief Set the input value of the port.
+     * @param value Input value to set.
      */
-    void setVoltage(double volt) { voltage = volt; }
+    void setInValue(double value) { inValue = value; }
 
     /**
-     * @brief Method to get the voltage of the port.
-     * @return Current voltage of the port.
+     * @brief Get the input value of the port.
+     * @return Input value of the port.
      */
-    double getVoltage() const { return voltage; }
+    double getInValue() const { return inValue; }
 
     /**
-     * @brief Method to connect an input connection.
-     * @param connection Connection to be set as input.
+     * @brief Get the output value of the port.
+     * @return Output value of the port.
      */
-    void connectIn(const Connection& connection) { inPort = connection; }
+    double getOutValue() const { return outValue; }
 
     /**
-     * @brief Method to connect an output connection.
-     * @param connection Connection to be set as output.
+     * @brief Toggle the port state based on the coil state.
+     * @param isOn Coil state; if true, set outValue to inValue, else set outValue to 0.
      */
-    void connectOut(const Connection& connection) { outPort = connection; }
-
-    /**
-     * @brief Method to disconnect the input connection.
-     */
-    void disconnectIn() { inPort = Connection(-1, ""); }
-
-    /**
-     * @brief Method to disconnect the output connection.
-     */
-    void disconnectOut() { outPort = Connection(-1, ""); }
-
-    /**
-     * @brief Method to check if the port is connected.
-     * @return True if either input or output connection is valid, otherwise false.
-     */
-    bool isConnected() const { return inPort.getId() != -1 || outPort.getId() != -1; }
-
-    /**
-     * @brief Getter for input port.
-     * @return Input port.
-     */
-    Connection getInPort() const { return inPort; }
-
-    /**
-     * @brief Getter for output port.
-     * @return Output port.
-     */
-    Connection getOutPort() const { return outPort; }
+    void togglePort(bool isOn) {
+        if (isOn) {
+            outValue = inValue;
+        } else {
+            outValue = 0.0;
+        }
+    }
 };
 
 #endif // PORT_H
