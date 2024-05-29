3a7f40138b3eda57f64b1a77be2cfc9398ce82d4
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 15:21:56 +0530

    Update pt.cpp

--- pt/pt.cpp
+++ pt/pt.cpp
@@ -1,1 +1,18 @@
+// PotentialTransformer.cpp
+
+#include "PotentialTransformer.h"
+
+PotentialTransformer::PotentialTransformer(double primaryV, double turns) {
+    primaryVoltage = primaryV;
+    turnsRatio = turns;
+    secondaryVoltage = primaryVoltage / turnsRatio;
+}
+
+double PotentialTransformer::getPrimaryVoltage() {
+    return primaryVoltage;
+}
+
+double PotentialTransformer::getSecondaryVoltage() {
+    return secondaryVoltage;
+}
 
