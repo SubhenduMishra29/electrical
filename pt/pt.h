286bb6a01b8a5eddbb66289f6185e74cc1061f36
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 15:22:15 +0530

    Update pt.h

--- pt/pt.h
+++ pt/pt.h
@@ -1,1 +1,19 @@
+// PotentialTransformer.h
+
+#ifndef POTENTIAL_TRANSFORMER_H
+#define POTENTIAL_TRANSFORMER_H
+
+class PotentialTransformer {
+private:
+    double primaryVoltage;
+    double secondaryVoltage;
+    double turnsRatio;
+
+public:
+    PotentialTransformer(double primaryV, double turns);
+    double getPrimaryVoltage();
+    double getSecondaryVoltage();
+};
+
+#endif
 
