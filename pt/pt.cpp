0b43b2312b34db0f452e7636f83ed6ce3dc4d13e
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 15:39:46 +0530

    Update pt.cpp

--- pt/pt.cpp
+++ pt/pt.cpp
@@ -3,6 +3,10 @@
 #include "PotentialTransformer.h"
 
 PotentialTransformer::PotentialTransformer(double primaryV, double turns) {
+    if (turns <= 0) {
+        throw std::invalid_argument("Turns ratio must be greater than zero");
+    }
+    
     primaryVoltage = primaryV;
     turnsRatio = turns;
     secondaryVoltage = primaryVoltage / turnsRatio;
