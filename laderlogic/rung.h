598cef6a10ab2c7fb381e34cf6b79d8755c95719
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 16:08:58 +0530

    Update rung.h

--- laderlogic/rung.h
+++ laderlogic/rung.h
@@ -1,1 +1,19 @@
 // rung.h
+#ifndef RUNG_H
+#define RUNG_H
+
+#include <string>
+#include <vector>
+
+class Rung {
+private:
+    std::vector<std::pair<std::string, bool>> elements;
+
+public:
+    Rung();
+    void addElement(std::string name, bool state);
+    bool evaluate(std::unordered_map<std::string, bool>& input_states);
+};
+
+#endif // RUNG_H
+
