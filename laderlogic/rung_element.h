56380a8f9220a4c133efa24d09fb1e0df8e9c823
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 17:33:10 +0530

    Update rung_element.h

--- laderlogic/rung_element.h
+++ laderlogic/rung_element.h
@@ -7,6 +7,7 @@
 class RungElement {
 public:
     virtual bool evaluate(std::unordered_map<std::string, bool>& states) = 0;
+    virtual std::string getName() const = 0;
     virtual ~RungElement() = default;
 };
 
