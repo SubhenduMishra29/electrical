d71285ccec541010394eda1a73e5c5f578e5484f
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 17:27:12 +0530

    Update rung.cpp

--- laderlogic/rung.cpp
+++ laderlogic/rung.cpp
@@ -1,23 +1,29 @@
-// rung.cpp
 #include "rung.h"
-#include <unordered_map>
 
-Rung::Rung() {}
-
-void Rung::addElement(std::string name, bool state) {
-    elements.push_back(std::make_pair(name, state));
+void Rung::addElement(std::shared_ptr<RungElement> element) {
+    elements.push_back(element);
 }
 
-bool Rung::evaluate(std::unordered_map<std::string, bool>& input_states) {
+bool Rung::evaluate(std::unordered_map<std::string, bool>& states) {
     bool result = true;
+
     for (auto& element : elements) {
-        if (input_states.find(element.first) != input_states.end()) {
-            if (input_states[element.first] != element.second) {
-                result = false;
-                break;
-            }
-        }
+        result = result && element->evaluate(states);
     }
+
     return result;
 }
+
+void Rung::resetTimers() {
+    for (auto& element : elements) {
+        if (auto timer = std::dynamic_pointer_cast<TimerElement>(element)) {
+            timer->reset();
+        }
+    }
+}
+
+std::vector<std::shared_ptr<RungElement>>& Rung::getElements() {
+    return elements;
+}
+
 
