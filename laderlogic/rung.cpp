8c92269870b8b86679dce302770bf2d7a470d3db
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 16:09:28 +0530

    Update rung.cpp

--- laderlogic/rung.cpp
+++ laderlogic/rung.cpp
@@ -1,1 +1,23 @@
 // rung.cpp
+#include "rung.h"
+#include <unordered_map>
+
+Rung::Rung() {}
+
+void Rung::addElement(std::string name, bool state) {
+    elements.push_back(std::make_pair(name, state));
+}
+
+bool Rung::evaluate(std::unordered_map<std::string, bool>& input_states) {
+    bool result = true;
+    for (auto& element : elements) {
+        if (input_states.find(element.first) != input_states.end()) {
+            if (input_states[element.first] != element.second) {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
