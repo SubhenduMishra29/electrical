bcfdf43ac443c3470964309962047379285289bd
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-29 17:23:40 +0530

    Create rung_element.h

---
+++ laderlogic/rung_element.h
@@ -0,0 +1,13 @@
+#ifndef RUNG_ELEMENT_H
+#define RUNG_ELEMENT_H
+
+#include <string>
+#include <unordered_map>
+
+class RungElement {
+public:
+    virtual bool evaluate(std::unordered_map<std::string, bool>& states) = 0;
+    virtual ~RungElement() = default;
+};
+
+#endif // RUNG_ELEMENT_H
