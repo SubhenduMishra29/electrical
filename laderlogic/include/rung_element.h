96104d47c2317d28f23ea685db637347b73792d2
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 15:07:29 +0530

    Update rung_element.h

--- laderlogic/include/rung_element.h
+++ laderlogic/include/rung_element.h
@@ -1,3 +1,10 @@
+/*
+ * File: rung_element.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of RungElement class, the base class for elements in a ladder logic rung.
+ * License: GPL (General Public License)
+ */
+
 #ifndef RUNG_ELEMENT_H
 #define RUNG_ELEMENT_H
 
@@ -6,9 +13,15 @@
 
 class RungElement {
 public:
+    // Evaluate the rung element based on the current states.
     virtual bool evaluate(std::unordered_map<std::string, bool>& states) = 0;
+
+    // Get the name of the rung element.
     virtual std::string getName() const = 0;
+
+    // Virtual destructor for proper polymorphic behavior.
     virtual ~RungElement() = default;
 };
 
 #endif // RUNG_ELEMENT_H
+
