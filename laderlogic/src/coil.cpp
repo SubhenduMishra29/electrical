58ed4c2f1b35a0b943bd6cf75d23c17f42331c56
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 14:59:51 +0530

    Update coil.cpp

--- laderlogic/src/coil.cpp
+++ laderlogic/src/coil.cpp
@@ -1,11 +1,20 @@
+// File: coil.cpp
+// Author: Subhendu Mishra
+// Description: Implementation of the Coil class representing a coil element in ladder logic programming.
+// License: GPL (General Public License)
+
 #include "coil.h"
 
+// Constructor initializes the coil element with a name.
 Coil::Coil(std::string name) : name(name) {}
 
+// Evaluate the coil element based on the current states.
 bool Coil::evaluate(std::unordered_map<std::string, bool>& states) {
+    // Retrieve the state of the input associated with this coil from the states map.
     return states[name];
 }
 
+// Get the name of the coil element.
 std::string Coil::getName() const {
     return name;
 }
