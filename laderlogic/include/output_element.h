3713725db6579f353bc8a004504d8481220cbb0d
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 15:02:02 +0530

    Update output_element.h

--- laderlogic/include/output_element.h
+++ laderlogic/include/output_element.h
@@ -1,18 +1,35 @@
+/*
+ * File: output_element.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of OutputElement class representing an output element in ladder logic programming.
+ * License: [License Type]
+ */
+
 #ifndef OUTPUT_ELEMENT_H
 #define OUTPUT_ELEMENT_H
 
 #include "rung_element.h"
+#include <unordered_map>
+#include <string>
 
 class OutputElement : public RungElement {
 private:
-    std::string name;
-    bool state;
+    std::string name;   // Name of the output element
+    bool state;         // Current state of the output element
 
 public:
+    // Constructor initializes the output element with a name and initial state.
     OutputElement(std::string name, bool state);
+
+    // Evaluate the output element based on the current states.
     bool evaluate(std::unordered_map<std::string, bool>& states) override;
+
+    // Get the name of the output element.
     std::string getName();
+
+    // Get the state of the output element.
     bool getState();
 };
 
 #endif // OUTPUT_ELEMENT_H
+
