403a4c81b475013ed6a323e6f77033deffa1d71b
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 15:08:18 +0530

    Update rung.h

--- laderlogic/include/rung.h
+++ laderlogic/include/rung.h
@@ -1,3 +1,10 @@
+/*
+ * File: rung.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of Rung class representing a rung in a ladder logic program.
+ * License: GPL (General Public License)
+ */
+
 #ifndef RUNG_H
 #define RUNG_H
 
@@ -8,12 +15,18 @@
 
 class Rung {
 private:
-    std::vector<std::shared_ptr<RungElement>> elements;
+    std::vector<std::shared_ptr<RungElement>> elements; // Vector to store elements of the rung
 
 public:
+    // Add an element to the rung.
     void addElement(std::shared_ptr<RungElement> element);
+
+    // Evaluate the rung based on the current states.
     bool evaluate(std::unordered_map<std::string, bool>& states);
+
+    // Get the elements of the rung.
     std::vector<std::shared_ptr<RungElement>>& getElements();
 };
 
 #endif // RUNG_H
+
