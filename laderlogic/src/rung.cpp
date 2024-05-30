d3763d3de4dbf2e3004bf2ba0d6b273c572cf4f5
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 14:54:30 +0530

    Update rung.cpp

--- laderlogic/src/rung.cpp
+++ laderlogic/src/rung.cpp
@@ -1,17 +1,31 @@
-#include "rung.h"
+/*
+ * File: rung.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of Rung class for representing a ladder logic rung.
+ * License: GPL (General Public License)
+ */
 
-void Rung::addElement(std::shared_ptr<RungElement> element) {
-    elements.push_back(element);
-}
+#ifndef RUNG_H
+#define RUNG_H
 
-bool Rung::evaluate(std::unordered_map<std::string, bool>& states) {
-    bool result = true;
-    for (auto& element : elements) {
-        result = result && element->evaluate(states);
-    }
-    return result;
-}
+#include <memory>
+#include <unordered_map>
+#include <vector>
+#include "rung_element.h"
 
-std::vector<std::shared_ptr<RungElement>>& Rung::getElements() {
-    return elements;
-}
+class Rung {
+private:
+    std::vector<std::shared_ptr<RungElement>> elements;
+
+public:
+    // Add an element to the rung.
+    void addElement(std::shared_ptr<RungElement> element);
+
+    // Evaluate the rung based on the current states.
+    bool evaluate(std::unordered_map<std::string, bool>& states);
+
+    // Get the elements of the rung.
+    std::vector<std::shared_ptr<RungElement>>& getElements();
+};
+
+#endif /* RUNG_H */
