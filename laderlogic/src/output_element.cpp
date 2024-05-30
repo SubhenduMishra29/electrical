ea8f06adc2597a681d28e5453f3035d873581741
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 14:56:06 +0530

    Update output_element.cpp

--- laderlogic/src/output_element.cpp
+++ laderlogic/src/output_element.cpp
@@ -1,15 +1,34 @@
-#include "output_element.h"
+/*
+ * File: output_element.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of OutputElement class for representing an output element.
+ * License: GPL (General Public License)
+ */
 
-OutputElement::OutputElement(std::string name, bool state) : name(name), state(state) {}
+#ifndef OUTPUT_ELEMENT_H
+#define OUTPUT_ELEMENT_H
 
-bool OutputElement::evaluate(std::unordered_map<std::string, bool>& states) {
-    return state;
-}
+#include <string>
+#include <unordered_map>
+#include "rung_element.h"
 
-std::string OutputElement::getName() {
-    return name;
-}
+class OutputElement : public RungElement {
+private:
+    std::string name;
+    bool state;
 
-bool OutputElement::getState() {
-    return state;
-}
+public:
+    // Constructor initializes the output element with a name and initial state.
+    OutputElement(std::string name, bool state);
+
+    // Evaluate the output element and return its state.
+    bool evaluate(std::unordered_map<std::string, bool>& states) override;
+
+    // Get the name of the output element.
+    std::string getName() override;
+
+    // Get the state of the output element.
+    bool getState();
+};
+
+#endif /* OUTPUT_ELEMENT_H */
