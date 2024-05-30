fcf0d299bd8eb3e664a940384063ab560ab182f8
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-05-30 14:55:17 +0530

    Update pushbutton.cpp

--- laderlogic/src/pushbutton.cpp
+++ laderlogic/src/pushbutton.cpp
@@ -1,16 +1,34 @@
-#include "pushbutton.h"
+/*
+ * File: pushbutton.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of PushButton class for representing a push button element.
+ * License: GPL (General Public License)
+ */
 
-PushButton::PushButton(std::string name, bool state) : name(name), state(state) {}
+#ifndef PUSHBUTTON_H
+#define PUSHBUTTON_H
 
-bool PushButton::evaluate(std::unordered_map<std::string, bool>& states) {
-    states[name] = state;
-    return state;
-}
+#include <string>
+#include <unordered_map>
+#include "rung_element.h"
 
-std::string PushButton::getName() const {
-    return name;
-}
+class PushButton : public RungElement {
+private:
+    std::string name;
+    bool state;
 
-void PushButton::setState(bool state) {
-    this->state = state;
-}
+public:
+    // Constructor initializes the push button with a name and initial state.
+    PushButton(std::string name, bool state);
+
+    // Evaluate the push button and update its state in the states map.
+    bool evaluate(std::unordered_map<std::string, bool>& states) override;
+
+    // Get the name of the push button.
+    std::string getName() const;
+
+    // Set the state of the push button.
+    void setState(bool state);
+};
+
+#endif /* PUSHBUTTON_H */
