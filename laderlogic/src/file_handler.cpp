18ba872e91a464c1e3db95071f03ef0cc8bdd681
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-03 14:59:35 +0530

    Update file_handler.cpp

--- laderlogic/src/file_handler.cpp
+++ laderlogic/src/file_handler.cpp
@@ -1,28 +1,19 @@
 #include "file_handler.h"
 #include <fstream>
-#include <sstream>
 
-std::unordered_map<std::string, bool> FileHandler::parseInputFile(const std::string& filename) {
-    std::unordered_map<std::string, bool> elements;
-
+std::vector<std::string> FileHandler::readLines(const std::string& filename) {
+    std::vector<std::string> lines;
     std::ifstream file(filename);
     if (!file.is_open()) {
         // Handle error opening file
-        return elements;
+        return lines;
     }
 
     std::string line;
     while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::string elementType, elementName;
-        bool initialState;
-
-        iss >> elementType >> elementName >> initialState;
-
-        elements[elementName] = initialState;
+        lines.push_back(line);
     }
 
     file.close();
-
-    return elements;
+    return lines;
 }
