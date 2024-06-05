17b5eea0309bab18b94ea56ab1841ff08194faed
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-05 15:08:59 +0530

    Create rung_submodule.h
    This class manages a collection of RungElement objects, providing methods to add elements and evaluate the entire submodule.

---
+++ laderlogic/rung_submodule.h
@@ -0,0 +1,31 @@
+/*
+ * File: rung_submodule.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of RungSubmodule class for representing a submodule in a ladder logic rung.
+ * License: GPL (General Public License)
+ */
+
+#ifndef RUNG_SUBMODULE_H
+#define RUNG_SUBMODULE_H
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+#include "rung_element.h"
+
+class RungSubmodule {
+private:
+    std::vector<std::shared_ptr<RungElement>> elements;
+
+public:
+    // Add an element to the submodule.
+    void addElement(std::shared_ptr<RungElement> element);
+
+    // Evaluate the submodule based on the current states.
+    bool evaluate(std::unordered_map<std::string, bool>& states);
+
+    // Get the elements of the submodule.
+    std::vector<std::shared_ptr<RungElement>>& getElements();
+};
+
+#endif /* RUNG_SUBMODULE_H */
