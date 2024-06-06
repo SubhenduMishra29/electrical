8a97e7b2101bb699a9ee8c1844f65ab6b17c92c4
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-06 12:27:50 +0530

    Update rung.h

--- laderlogic/include/rung.h
+++ laderlogic/include/rung.h
@@ -8,22 +8,28 @@
 #ifndef RUNG_H
 #define RUNG_H
 
-#include <memory>
-#include <unordered_map>
 #include <vector>
+#include <memory>
 #include "rung_element.h"
-#include "pushbutton.h"
-#include "contact.h"
-#include "coil.h"
 
 class Rung {
 private:
-    std::vector<std::shared_ptr<RungElement>> elements;
+    std::vector<std::shared_ptr<RungElement>> positiveRail; // Positive rail of the rung
+    std::vector<std::shared_ptr<RungElement>> negativeRail; // Negative rail of the rung
 
 public:
+    /*
+     * Add an element to the rung, adding it to the positive rail
+     * @param element: The element to add to the rung
+     */
     void addElement(std::shared_ptr<RungElement> element);
+
+    /*
+     * Evaluate the rung based on the current states of inputs
+     * @param states: The states of inputs to evaluate against
+     * @return: The evaluation result of the rung
+     */
     bool evaluate(std::unordered_map<std::string, bool>& states);
-    std::vector<std::shared_ptr<RungElement>>& getElements();
 };
 
 #endif // RUNG_H
