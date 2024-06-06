b86c48fdb526f3cdbc4d3551411e23e3f66eee20
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-06 13:19:12 +0530

    Update bulb.cpp

--- laderlogic/src/bulb.cpp
+++ laderlogic/src/bulb.cpp
@@ -3,18 +3,19 @@
 // Description: Implementation of the Bulb class representing a bulb element in ladder logic programming.
 // License: GPL (General Public License)
 
+// bulb.cpp
 #include "bulb.h"
 
-// Constructor initializes the bulb element with a name.
 Bulb::Bulb(std::string name) : name(name) {}
 
-// Evaluate the bulb element based on the current states.
 bool Bulb::evaluate(std::unordered_map<std::string, bool>& states) {
-    // Retrieve the state of the input associated with this bulb from the states map.
     return states[name];
 }
 
-// Get the name of the bulb element.
 std::string Bulb::getName() const {
     return name;
+}
+
+bool Bulb::isTerminatingElement() const {
+    return true; // Bulb is a terminating element
 }
