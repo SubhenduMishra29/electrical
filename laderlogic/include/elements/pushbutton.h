d6c3e02cef2ec233b7070547a19fc25ba11c511f
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-06 14:12:41 +0530

    Update pushbutton.h

--- laderlogic/include/elements/pushbutton.h
+++ laderlogic/include/elements/pushbutton.h
@@ -1,18 +1,31 @@
+// pushbutton.h
 #ifndef PUSHBUTTON_H
 #define PUSHBUTTON_H
 
 #include "rung_element.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+
+class Wire; // Forward declaration of Wire class
 
 class PushButton : public RungElement {
 private:
     std::string name;
     bool state;
+    std::vector<std::shared_ptr<Wire>> connections; // Vector to store connections
 
 public:
     PushButton(std::string name, bool state);
     bool evaluate(std::unordered_map<std::string, bool>& states) override;
     std::string getName() const override;
     void setState(bool state);
+
+    // Methods for managing connections
+    void connectTo(std::shared_ptr<RungElement> element) override;
+    void disconnectFrom(std::shared_ptr<RungElement> element) override;
+    bool isConnectedTo(std::shared_ptr<RungElement> element) const override;
 };
 
 #endif // PUSHBUTTON_H
