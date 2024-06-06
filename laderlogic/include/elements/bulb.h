3ff46fd5c0c6231f509355c094978f6ce900c59b
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-06 13:18:46 +0530

    Update bulb.h

--- laderlogic/include/elements/bulb.h
+++ laderlogic/include/elements/bulb.h
@@ -1,3 +1,4 @@
+// bulb.h
 #ifndef BULB_H
 #define BULB_H
 
@@ -11,6 +12,8 @@ public:
     Bulb(std::string name);
     bool evaluate(std::unordered_map<std::string, bool>& states) override;
     std::string getName() const override;
+    bool isTerminatingElement() const override; // Method to check if the Bulb is a terminating element
 };
 
 #endif // BULB_H
+
