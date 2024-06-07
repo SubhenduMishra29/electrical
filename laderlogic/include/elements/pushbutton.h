a92dbd36e8abe48ae4384ce9ea22c2ce79152a4a
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-07 12:20:02 +0530

    Update pushbutton.h

--- laderlogic/include/elements/pushbutton.h
+++ laderlogic/include/elements/pushbutton.h
@@ -1,4 +1,10 @@
-// pushbutton.h
+/*
+ * File: pushbutton.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of the PushButton class representing a push button element in ladder logic programming.
+ * License: GPL (General Public License)
+ */
+
 #ifndef PUSHBUTTON_H
 #define PUSHBUTTON_H
 
@@ -17,9 +23,16 @@ private:
     std::vector<std::shared_ptr<Wire>> connections; // Vector to store connections
 
 public:
+    // Constructor initializes the push button element with a name and initial state.
     PushButton(std::string name, bool state);
+
+    // Evaluate the push button element based on the current states.
     bool evaluate(std::unordered_map<std::string, bool>& states) override;
+
+    // Get the name of the push button element.
     std::string getName() const override;
+
+    // Set the state of the push button element.
     void setState(bool state);
 
     // Methods for managing connections
