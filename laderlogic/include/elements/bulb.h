7da46b1e7a9c29bffadcd2e1f6a227d22fc81c0d
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-07 12:18:23 +0530

    Update bulb.h

--- laderlogic/include/elements/bulb.h
+++ laderlogic/include/elements/bulb.h
@@ -1,4 +1,10 @@
-// bulb.h
+/*
+ * File: bulb.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of the Bulb class representing a bulb element in ladder logic programming.
+ * License: GPL (General Public License)
+ */
+
 #ifndef BULB_H
 #define BULB_H
 
@@ -9,10 +15,17 @@ private:
     std::string name;
 
 public:
+    // Constructor initializes the bulb element with a name.
     Bulb(std::string name);
+
+    // Evaluate the bulb element based on the current states.
     bool evaluate(std::unordered_map<std::string, bool>& states) override;
+
+    // Get the name of the bulb element.
     std::string getName() const override;
-    bool isTerminatingElement() const override; // Method to check if the Bulb is a terminating element
+
+    // Method to check if the Bulb is a terminating element.
+    bool isTerminatingElement() const override;
 };
 
 #endif // BULB_H
