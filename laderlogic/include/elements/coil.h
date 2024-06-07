55ac0f2b5915dd1701ddec8eb664d0fd19eddb2a
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-07 12:18:50 +0530

    Update coil.h

--- laderlogic/include/elements/coil.h
+++ laderlogic/include/elements/coil.h
@@ -1,3 +1,10 @@
+/*
+ * File: coil.h
+ * Author: Subhendu Mishra
+ * Description: Declaration of the Coil class representing a coil element in ladder logic programming.
+ * License: GPL (General Public License)
+ */
+
 #ifndef COIL_H
 #define COIL_H
 
@@ -8,8 +15,13 @@ private:
     std::string name;
 
 public:
+    // Constructor initializes the coil element with a name.
     Coil(std::string name);
+
+    // Evaluate the coil element based on the current states.
     bool evaluate(std::unordered_map<std::string, bool>& states) override;
+
+    // Get the name of the coil element.
     std::string getName() const override;
 };
 
