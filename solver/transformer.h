81a3c03975c3b15b9af2f4dc03d6a269bbfa079d
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-18 15:49:21 +0530

    Create transformer.h

---
+++ solver/transformer.h
@@ -0,0 +1,17 @@
+#ifndef TRANSFORMER_H
+#define TRANSFORMER_H
+
+#include "bus.h"
+
+class Transformer {
+private:
+    Bus* primaryBus;
+    Bus* secondaryBus;
+    double turnsRatio;
+
+public:
+    Transformer(Bus* primaryBus, Bus* secondaryBus, double turnsRatio);
+    void applyTurnsRatio();
+};
+
+#endif // TRANSFORMER_H
