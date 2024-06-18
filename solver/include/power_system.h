f52fe00d2723432037de2d3fd2c52125279e3398
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-18 15:53:27 +0530

    Update power_system.h

--- solver/include/power_system.h
+++ solver/include/power_system.h
@@ -1,18 +1,18 @@
-// power_system.h
-
 #ifndef POWER_SYSTEM_H
 #define POWER_SYSTEM_H
 
 #include <vector>
-#include "bus.h"  // Include bus header
+#include "bus.h"
 
 class PowerSystem {
 private:
     std::vector<Bus*> buses;
+    std::vector<Transformer*> transformers; // Vector to hold system transformers
 
 public:
     PowerSystem();
     void addBus(Bus* bus);
+    void addTransformer(Transformer* transformer); // Method to add transformer to system
     void runSimulation();
 };
 
