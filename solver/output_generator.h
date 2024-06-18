23bfe4eb2a05cb46d08a9af8cefa6b087a73e326
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-18 15:08:35 +0530

    Create output_generator.h

---
+++ solver/output_generator.h
@@ -0,0 +1,12 @@
+// output_generator.h
+#ifndef OUTPUT_GENERATOR_H
+#define OUTPUT_GENERATOR_H
+
+#include "power_system.h"
+
+class OutputGenerator {
+public:
+    static void generateReport(const PowerSystem& powerSystem, const std::string& filename);
+};
+
+#endif // OUTPUT_GENERATOR_H
