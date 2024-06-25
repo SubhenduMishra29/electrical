a62d3c594b54212416b2781d8c9bee3f9e7aa997
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-25 13:28:47 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
         return false; // Return false to stop the timeout after the first invocation
     }, 8000); // 8000 milliseconds = 8 seconds
 
-    // Run the application main loop
+    // Run the application main loop with splashScreen
     int status = app->run(splashScreen);
 
     if (status != 0) {
@@ -25,12 +25,45 @@ int main(int argc, char *argv[]) {
     // Create main application window
     Gtk::Window mainWindow;
     mainWindow.set_title("Main Application");
+    mainWindow.set_default_size(800, 600); // Set a default size for the window
 
-    // Create an instance of GuiComponent
-    GuiComponent guiComponent;
-    mainWindow.add(guiComponent);
+    // Create a vertical box container for main window
+    Gtk::Box mainBox(Gtk::ORIENTATION_VERTICAL);
+    mainWindow.add(mainBox);
 
-    // Show window and run application
+    // Create a menu bar
+    Gtk::MenuBar menuBar;
+    Gtk::MenuItem menuFile("File");
+    Gtk::Menu menu;
+    Gtk::MenuItem menuExit("Exit");
+    menu.append(menuExit);
+    menuFile.set_submenu(menu);
+    menuBar.append(menuFile);
+    mainBox.pack_start(menuBar, Gtk::PACK_SHRINK);
+
+    // Handle exit menu item
+    menuExit.signal_activate().connect([&]() {
+        mainWindow.close();
+    });
+
+    // Create a label
+    Gtk::Label label("Welcome to Main Window!");
+    label.set_margin_top(20);
+    label.set_margin_bottom(20);
+    label.set_halign(Gtk::ALIGN_CENTER);
+    mainBox.pack_start(label, Gtk::PACK_SHRINK);
+
+    // Create a button
+    Gtk::Button button("Click Me!");
+    button.signal_clicked().connect([&]() {
+        Gtk::MessageDialog dialog(*mainWindow, "Button Clicked!", false, Gtk::MESSAGE_INFO, Gtk::BUTTONS_OK, true);
+        dialog.run();
+    });
+    mainBox.pack_start(button, Gtk::PACK_SHRINK);
+
+    // Show all widgets
     mainWindow.show_all();
+
+    // Run the application main loop with mainWindow
     return app->run(mainWindow);
 }
