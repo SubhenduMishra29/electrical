6b8a5d3fda7d0886f0063a58d7c37da4f3a54650
SubhenduMishra29 <51118046+SubhenduMishra29@users.noreply.github.com>
2024-06-26 18:38:26 +0530

    Update transformer.h

--- solver/include/transformer.h
+++ solver/include/transformer.h
@@ -1,6 +1,7 @@
 #ifndef TRANSFORMER_H
 #define TRANSFORMER_H
 
+#include <string>
 #include "bus.h"
 #include "PowerSystemError.h"
 
@@ -9,10 +10,56 @@ private:
     Bus* primaryBus;
     Bus* secondaryBus;
     double turnsRatio;
+    double impedance;
+    double primaryVoltageRating;
+    double secondaryVoltageRating;
+    double powerRating;
+    double noLoadLosses;
+    double loadLosses;
+    double tapSetting;
 
 public:
+    // Basic constructor
     Transformer(Bus* primaryBus, Bus* secondaryBus, double turnsRatio);
-    void applyTurnsRatio();
+
+    // Detailed constructor
+    Transformer(Bus* primaryBus, Bus* secondaryBus, double turnsRatio, double impedance, double primaryVoltageRating, double secondaryVoltageRating, double powerRating, double noLoadLosses, double loadLosses, double tapSetting);
+
+    // Getters and setters
+    Bus* getPrimaryBus() const;
+    void setPrimaryBus(Bus* bus);
+
+    Bus* getSecondaryBus() const;
+    void setSecondaryBus(Bus* bus);
+
+    double getTurnsRatio() const;
+    void setTurnsRatio(double ratio);
+
+    double getImpedance() const;
+    void setImpedance(double impedance);
+
+    double getPrimaryVoltageRating() const;
+    void setPrimaryVoltageRating(double voltage);
+
+    double getSecondaryVoltageRating() const;
+    void setSecondaryVoltageRating(double voltage);
+
+    double getPowerRating() const;
+    void setPowerRating(double power);
+
+    double getNoLoadLosses() const;
+    void setNoLoadLosses(double losses);
+
+    double getLoadLosses() const;
+    void setLoadLosses(double losses);
+
+    double getTapSetting() const;
+    void setTapSetting(double tap);
+
+    // Other methods
+    void applyTurnsRatio() const;
+    void calculateEfficiency() const;
+    void adjustTap(double newTapSetting);
 };
 
 #endif // TRANSFORMER_H
